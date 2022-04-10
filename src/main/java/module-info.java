ee7d2e601e52d81ee88b0a801c298f3b63236f23
d.stueken <d.stueken@conterra.de>
2022-04-10 18:58:57 +0200

    switched to gradle

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+open module fx3d {
+    requires javafx.controls;
+
+    exports gc.tutorial.chapt1;
+    exports gc.tutorial.chapt2;
+    exports gc.tutorial.chapt3;
+    exports gc.tutorial.chapt4;
+    exports gc.tutorial.chapt5;
+    exports gc.tutorial.chapt6;
+    exports gc.tutorial.chapt7;
+    exports gc.tutorial.chapt8;
+    exports gc.tutorial.chapt9;
+    exports gc.tutorial.chapt10;
+    exports gc.tutorial.chapt11;
+    exports gc.tutorial.chapt12;
+    exports gc.tutorial.chapt13;
+    exports gc.tutorial.chapt14;
+    exports gc.tutorial.chapt15;
+    exports gc.tutorial.chapt16;
+    exports gc.tutorial.chapt17;
+}
